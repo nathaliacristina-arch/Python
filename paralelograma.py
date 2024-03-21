443d86711329413f0ca98bfd0001cd990765f228
Nathalia Cristina <83037660+nathaliacristina-arch@users.noreply.github.com>
2024-03-21 12:10:56 -0300

    Add files via upload

---
+++ paralelograma.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Pseudo código para calcular a area de um paralelograma
+
+#  Para calcular a área de um paralelograma, podemos seguir os seguintes passos:
+#  
+# 1-  Declare as variáveis  comprimento e altura.
+# 2-  Peça ao usuário para fornecer os valores de comprimento e altura.
+# 3- Calcule a área multiplicando comprimento pela altura.
+# 4- Armazene o resultado na variável "área".
+# 5- Imprima o valor da variável "área".
+
+# Aqui está um exemplo de pseudocódigo que implementa esses passos:
+#  Declare comprimento, altura, area
+#  Obtenha comprimento do usuário
+#  Obtenha altura do usuário
+#  area <- comprimento * altura
+#  Imprima "A área do paralelograma é:" + area
+
+# # Código em Python
+
+# In[1]:
+
+
+print ("Bem vindo ao calculador de área de Paralelogramo")
+
+
+# In[2]:
+
+
+Base = float (input("Insira o comprimento da base: "))
+
+
+# In[3]:
+
+
+Altura = float (input("Insira a altura: "))
+
+
+# In[4]:
+
+
+Area = Base + Altura
+
+
+# In[5]:
+
+
+print (" A area do paralelogramo é: ", Area)
+
+
+# In[ ]:
+
+
+
+
