c4038d5c6aabc3cf66d626b7b4737746c30e8011
Nathalia Cristina <83037660+nathaliacristina-arch@users.noreply.github.com>
2024-04-01 20:14:23 -0300

    Add files via upload

---
+++ Range.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Função Range
+# 
+# range() é usada para gerar uma sequência de números em um intervalo especificado
+# 
+# # Sintaxe 
+# 
+# A sintaxe básica da função range() é:
+# range(start, stop, step)
+
+# In[1]:
+
+
+for i in range (0, -20, -2):
+    print (i)
+    
+
+
+# No caso, imprime os números de 0 a -20, pulando de -2 em -2
