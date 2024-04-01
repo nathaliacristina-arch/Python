1817af534e2d57fd08c047fa5c5027b2c7d6c168
Nathalia Cristina <83037660+nathaliacristina-arch@users.noreply.github.com>
2024-04-01 19:45:59 -0300

    Add files via upload

---
+++ break_pass.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Se encontramos o número 4 interrompemos o loop
+
+valor = 0
+
+while valor < 10:
+    if valor == 4:
+      break
+    else:
+        pass
+    
+    print(valor)
+    valor = valor + 1
+   
+
+
+# In[ ]:
+
+
+
+
