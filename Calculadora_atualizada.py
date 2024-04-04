3496dfc067cf03d72c57acc14ff2626b347451b5
Nathalia Cristina <83037660+nathaliacristina-arch@users.noreply.github.com>
2024-04-04 19:34:40 -0300

    Add files via upload

---
+++ Calculadora_atualizada.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[57]:
+
+
+print ("Selecione o número da operação desejada: \n"
+       "1 - Soma \n"
+       "2 - Subtração \n"
+       "3 - Multiplicacao\n"
+       "4 - Divisão \n")
+
+
+operacao =   (input ("Digite sua opção : \n "))
+
+    
+
+
+# In[58]:
+
+
+num1 = int (input ( ("Digite o primeiro numero : " )))
+
+
+# In[59]:
+
+
+num2 = int  (input ( ("Digite o segundo numero : " )))
+
+
+# In[60]:
+
+
+if operacao == "1":
+    resultado = num1 + num2
+    print (num1, " + ", num2, " = " , resultado)
+
+elif operacao == "2" :
+    resultado = num1 - num2
+    print (num1, " - ", num2, " = " , resultado)
+    
+elif operacao == "3":
+    resultado = num1 * num2
+    print (num1, " * ", num2, " = " , resultado)
+
+else: 
+
+    resultado = num1 / num2
+    print (num1, " / ", num2, " = " , resultado)
+
+
+# In[ ]:
+
+
+
+
